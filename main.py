675623beeebed9f598ddfaf087597fe3adba0472
0324bence <d.bence0324@gmail.com>
2024-07-23 11:59:01 +0200

    Add arguments and configurability

--- main.py
+++ main.py
@@ -1,14 +1,57 @@
 from PIL import Image
 import os
 import re
+import getopt, sys
 
-# file path (later change to user input)
-path = "./images/"
+def_path = "./images/"
+def_file_name = "^([\\d]*)_([\\d]*)"
 
-# file name regex (later change to user input)
-file_name = "^([\d]*)_([\d]*)"
+path = ""
+file_name = ""
+
+arguments = sys.argv[1:]
+
+options = "hp:f:"
+
+long_options = ["help", "path=", "file_name="]
+
+try:
+    arguments, values = getopt.getopt(arguments, options, long_options)
+
+    for current_argument, current_value in arguments:
+        if current_argument in ("-h", "--help"):
+            print("Usage: python main.py -p <path> -f <file_name>")
+            print("Path: path to the folder containing the images (default: ./images/)")
+            print("File_name: regex to match the file name (default: ^([\\d]*)_([\\d]*)")
+            sys.exit(0)
+        elif current_argument in ("-p", "--path"):
+            path = current_value
+        elif current_argument in ("-f", "--file_name"):
+            file_name = current_value
+except getopt.error as err:
+    print(str(err))
+    sys.exit(2)
+
+if path == "":
+    inp = input("Path to the folder containing the images (default: ./images/): ")
+    if inp == "":
+        path = def_path
+    else:
+        path = inp
+
+if file_name == "":
+    inp = input("Regex to match the file name (default: ^([\\d]*)_([\\d]*): ")
+    if inp == "":
+        file_name = def_file_name
+    else:
+        file_name = inp
+
 
 files = os.listdir(path)
+
+for file in files:
+    print("Found file: " + file)
+input("Press Enter to continue...")
 
 size = 0, 0
 
@@ -35,3 +78,6 @@ for file in files:
             result_image.paste(img, (x * size[0], y * size[1]))
 
 result_image.save("result.jpg")
+print("Result image saved as result.jpg")
+with result_image as img:
+    img.show()
